8daacc7522e19fc7e5ebbef32e5ced4d7c132b48
comjens <jens@garage.dk>
2018-01-02 19:41:59 +0100

    Made default folder

--- configuration.py
+++ configuration.py
@@ -6,31 +6,42 @@ dirname, filename = os.path.split(os.path.abspath(__file__))
 
 ### login
 if not os.path.isfile(dirname+"/lmtdAccss.txt"):
-	cnd.login()
+    cnd.login()
 else:
-	print("Configure login? (yes/no)")
-	configure = input()
-	if configure == "yes" or configure == "y":
-		cnd.login()
+    print("Configure login? (yes/no)")
+    configure = input()
+    if configure == "yes" or configure == "y":
+        cnd.login()
 
 ### Config
 print("Configure courses? (yes/no)")
 configure = input()
 if configure == "yes" or configure == "y":
-	# Create request and load XML into 'root'
-	url = 'https://www.campusnet.dtu.dk/data/CurrentUser/Elements'
-	response = cnd.sendRequest(url)
-	root = ET.fromstring(response.text)
-	# Run through 'root' node and save elementID, download path and versioning in config
-	f = open(dirname+'/config.txt','w')
-	for node in root:
-		for child in node:
-			print("Add \"%s --- %s\" to Downloads? (y/n)" % (node.get('Name'),child.get('Name')))
-			answer = input()
+    # Create request and load XML into 'root'
+    url = 'https://www.campusnet.dtu.dk/data/CurrentUser/Elements'
+    response = cnd.sendRequest(url)
+    root = ET.fromstring(response.text)
+    # Run through 'root' node and save elementID, download path and versioning in config
+    f = open(dirname+'/config.txt','w')
 
-			if answer=="yes" or answer=="y":
-				print("Where to download the \"%s\" files? (Path)" % (child.get('Name')))
-				directory = input()
-				directory = directory.replace("\\","/")
-				f.write(child.get('Name')+";"+child.get('Id')+";"+directory+"\n")
-	f.close()
+    print("Configure default Path? (yes/no)")
+    pathy = input()
+    if pathy == "yes" or pathy == "y":
+        print("Where to download the folders? (Path)")
+        path = input().replace("\\","/")
+
+    for node in root:
+        for child in node:
+            print("Add \"%s --- %s\" to Downloads? (y/n)" % (node.get('Name'),child.get('Name')))
+            answer = input()
+            if answer=="yes" or answer=="y":
+                print("Use default path for \"%s --- %s\" " % (node.get('Name'),child.get('Name')))
+                answer2 = input()
+                if answer2=="yes" or answer2=="y":
+                    f.write(child.get('Name')+";"+child.get('Id')+";"+path+"/"+child.get('Name').replace(" ","_")+"\n")
+                else:
+                    print("Where to download the \"%s\" files? (Path)" % (child.get('Name')))
+                    directory = input()
+                    directory = directory.replace("\\","/")
+                    f.write(child.get('Name')+";"+child.get('Id')+";"+directory+"\n")
+    f.close()
